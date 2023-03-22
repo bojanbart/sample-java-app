1ed4a7441a2be6917d1b3e57ce077f9e509e160c
bbojanowicz <bartosz.bojanowicz@lppsa.com>
2023-03-22 16:08:19 +0100

    java sample app - init commit

---
+++ src/main/java/bojan/sampleJavaApp/HelloWorldController.java
@@ -0,0 +1,13 @@
+package bojan.sampleJavaApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    String index() {
+        return "Hello World!";
+    }
+}
---
+++ src/main/java/bojan/sampleJavaApp/SampleJavaAppApplication.java
@@ -0,0 +1,13 @@
+package bojan.sampleJavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaAppApplication.class, args);
+	}
+
+}
